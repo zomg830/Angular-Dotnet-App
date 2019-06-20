f7c0f52a3b59ae7edd975861ab5077d92fc68c76
zomg830 <matt.faubel@gmail.com>
2019-06-20 12:42:33 -0500

    Added routing and route guards

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -12,7 +13,8 @@ export class NavComponent implements OnInit {
 
   constructor(
     public authService: AuthService,
-    private alertify: AlertifyService
+    private alertify: AlertifyService,
+    private router: Router
   ) {}
 
   ngOnInit() {}
@@ -22,7 +24,8 @@ export class NavComponent implements OnInit {
       next => {
         this.alertify.success('Logged in successfully');
       },
-      error => this.alertify.error(error)
+      error => this.alertify.error(error),
+      () => this.router.navigate(['/members'])
     );
   }
 
@@ -33,5 +36,6 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertify.message('Logged out');
+    this.router.navigate(['/home']);
   }
 }
