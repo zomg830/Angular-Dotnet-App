947e1b243de24e34427983a895bbde6660c79e70
zomg830 <matt.faubel@gmail.com>
2019-06-20 11:00:48 -0500

    Added error handling from server to client

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -18,7 +18,7 @@ export class NavComponent implements OnInit {
       next => {
         console.log('Logged in successfully');
       },
-      error => console.log('Failed to login')
+      error => console.log(error)
     );
   }
 
